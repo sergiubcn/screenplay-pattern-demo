4bcc2571fbae55c8ebe68885f430798f6f2f93d0
Sergiu Bacanu <sergiu.bacanu@moduscreate.com>
2018-10-15 16:51:02 +0300

    Revert "Setup wdio. Add initial files."
    This reverts commit 42f0be8facf83104cdadfedf87ee8fab49861d32.

--- pageObjects/page.js
+++
@@ -1,3 +0,0 @@
-module.exports = function page(path) {
-    browser.url(path);
-}
--- stepDefinitions/given.js
+++
@@ -1,5 +0,0 @@
-module.exports = function () {
-    this.Given(/^that ((a|an) (admin|subscriber)) user logs into WP-Admin$/, () => {
-        
-    });
-}
